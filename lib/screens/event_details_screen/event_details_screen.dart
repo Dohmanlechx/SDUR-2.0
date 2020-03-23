66072f89be1b7242c48b65c2c2771600da5c1c02
David Öhman <dohman_92@hotmail.com>
2020-03-23 22:08:39 +0100

    Refactor event_details_screen

--- lib/screens/event_details_screen/event_details_screen.dart
+++ lib/screens/event_details_screen/event_details_screen.dart
@@ -4,6 +4,7 @@ import 'package:sdur_app/models/sdur_event.dart';
 import 'package:sdur_app/resource/sdur_colors.dart';
 import 'package:sdur_app/resource/sdur_strings.dart';
 import 'package:sdur_app/screens/event_details_screen/event_details_views/event_item_info_column.dart';
+import 'package:sdur_app/views/sdur_dialog.dart';
 import 'package:sdur_app/views/sdur_scaffold.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -18,21 +19,11 @@ class EventDetailsScreen extends StatelessWidget {
     showDialog(
       context: ctx,
       builder: (BuildContext ctx) {
-        return AlertDialog(
-          title: Text(
-            SdurStrings.ERROR_DIALOG_TITLE,
-            style: Theme.of(ctx).textTheme.body2.copyWith(fontSize: 20, color: SdurColors.BLACK),
-          ),
-          content: Text(
-            SdurStrings.ERROR_DIALOG_MESSAGE,
-            style: Theme.of(ctx).textTheme.body1.copyWith(height: 1.25),
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: const Text(SdurStrings.WORD_OK),
-              onPressed: () => Navigator.of(ctx).pop(),
-            )
-          ],
+        return SdurDialog(
+          title: SdurStrings.ERROR_DIALOG_TITLE,
+          message: SdurStrings.ERROR_DIALOG_MESSAGE,
+          submitText: SdurStrings.WORD_OK,
+          onSubmit: () => Navigator.of(ctx).pop(),
         );
       },
     );
@@ -48,49 +39,61 @@ class EventDetailsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            GestureDetector(
-              onTap: () => Navigator.pop(context),
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                child: Hero(
-                  tag: "${sdurEvent.hashCode}",
-                  child: Container(
-                    width: 200,
-                    height: 200,
-                    margin: const EdgeInsets.only(top: 32),
-                    padding: const EdgeInsets.all(16),
-                    color: SdurColors.ACCENT,
-                    child: EventItemInfoColumn(
-                      dateTime: sdurEvent.dateTime,
-                      name: sdurEvent.title,
-                      targetGroup: sdurEvent.targetGroup,
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            _buildClickableSquare(context, sdurEvent),
             const SizedBox(height: 16),
-            Container(
-              color: SdurColors.ACCENT,
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Text(sdurEvent.description, style: Theme.of(context).textTheme.body1),
-              ),
-            ),
+            _buildDescription(context, sdurEvent),
             const SizedBox(height: 16),
-            Container(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: CupertinoButton(
-                  onPressed: () => _openInWebBrowser(context, sdurEvent.url),
-                  color: Colors.blue,
-                  child: const Text("ANMÄL MIG!"),
-                ),
-              ),
-            )
+            _buildButton(context, sdurEvent),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildClickableSquare(BuildContext ctx, SdurEvent sdurEvent) {
+    return GestureDetector(
+      onTap: () => Navigator.pop(ctx),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Hero(
+          tag: "${sdurEvent.hashCode}",
+          child: Container(
+            width: 200,
+            height: 200,
+            margin: const EdgeInsets.only(top: 32),
+            padding: const EdgeInsets.all(16),
+            color: SdurColors.ACCENT,
+            child: EventItemInfoColumn(
+              dateTime: sdurEvent.dateTime,
+              name: sdurEvent.title,
+              targetGroup: sdurEvent.targetGroup,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDescription(BuildContext ctx, SdurEvent sdurEvent) {
+    return Container(
+      color: SdurColors.ACCENT,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Text(sdurEvent.description, style: Theme.of(ctx).textTheme.body1),
+      ),
+    );
+  }
+
+  Widget _buildButton(BuildContext ctx, SdurEvent sdurEvent) {
+    return Container(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: CupertinoButton(
+          onPressed: () => _openInWebBrowser(ctx, sdurEvent.url),
+          color: Colors.blue,
+          child: const Text("ANMÄL MIG!"),
         ),
       ),
     );
---
+++ lib/views/sdur_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:sdur_app/resource/sdur_colors.dart';
+
+class SdurDialog extends StatelessWidget {
+  final String title;
+  final String message;
+  final String submitText;
+  final Function onSubmit;
+
+  const SdurDialog({
+    this.title,
+    this.message,
+    this.submitText,
+    this.onSubmit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.body2.copyWith(fontSize: 20, color: SdurColors.BLACK),
+      ),
+      content: Text(
+        message,
+        style: Theme.of(context).textTheme.body1.copyWith(height: 1.25),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text(submitText),
+          onPressed: onSubmit,
+        )
+      ],
+    );
+  }
+}
