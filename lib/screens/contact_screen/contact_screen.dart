d5d1d79edf4869f8ff1597814c9fadf9b278be07
David Öhman <dohman_92@hotmail.com>
2020-04-26 15:11:22 +0200

    Extract StaffRow widget into an own file

--- lib/screens/contact_screen/contact_screen.dart
+++ lib/screens/contact_screen/contact_screen.dart
@@ -1,10 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:sdur_app/resource/sdur_strings.dart';
-import 'package:sdur_app/util/device_info.dart';
+import 'package:sdur_app/screens/contact_screen/widgets/staff_row.dart';
 import 'package:sdur_app/views/sdur_scaffold.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ContactScreen extends StatelessWidget {
   static const routeName = "/contact_screen";
@@ -19,24 +17,21 @@ class ContactScreen extends StatelessWidget {
           children: <Widget>[
             _buildAddress(context),
             const SizedBox(height: 32),
-            _buildStaffRow(
-              context,
+            const StaffRow(
               imageUrl: "https://www.sdur.se/wp-content/uploads/2018/10/Florian.jpg",
               jobTitle: "Ungdomskonsulent",
               name: "Florian Tirnovan",
               email: "florian@sdur.se",
               phone: "0793477827",
             ),
-            _buildStaffRow(
-              context,
+            const StaffRow(
               imageUrl: "https://www.sdur.se/wp-content/uploads/2018/10/michelle.jpg",
               jobTitle: "Verksamhetsansvarig",
               name: "Michelle Malchow",
               email: "michelle@sdur.se",
               phone: "0703555998",
             ),
-            _buildStaffRow(
-              context,
+            const StaffRow(
               imageUrl: "https://www.sdur.se/wp-content/uploads/2019/03/jamila1.jpg",
               jobTitle: "Fritidsföreståndare (DUKiS)",
               name: "Jamila Ouahid",
@@ -63,62 +58,6 @@ class ContactScreen extends StatelessWidget {
           _buildAutoSizeText(SdurStrings.SDUR_EMAIL_ADDRESS),
         ],
       ),
-    );
-  }
-
-  Widget _buildStaffRow(
-    BuildContext ctx, {
-    String imageUrl,
-    String jobTitle,
-    String name,
-    String email,
-    String phone,
-  }) {
-    final TextStyle _textStyle = Theme.of(ctx).textTheme.body1.copyWith(height: 1.2);
-
-    Widget _buildAutoSizeText(String text, {bool isBold = false}) {
-      return AutoSizeText(
-        text,
-        style: _textStyle.copyWith(fontWeight: isBold ? FontWeight.bold : FontWeight.normal),
-        maxLines: 1,
-      );
-    }
-
-    return Column(
-      children: <Widget>[
-        GestureDetector(
-          onTap: () async => await launch("mailto:$email"),
-          child: Row(
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(4),
-                child: FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  fit: BoxFit.cover,
-                  image: imageUrl,
-                  width: DeviceInfo.width(ctx) * 0.35,
-                  height: DeviceInfo.height(ctx) * 0.125,
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 16, top: 16, bottom: 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      _buildAutoSizeText(jobTitle, isBold: true),
-                      _buildAutoSizeText(name),
-                      _buildAutoSizeText(email),
-                      _buildAutoSizeText(phone)
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-        const SizedBox(height: 16),
-      ],
     );
   }
 }
---
+++ lib/screens/contact_screen/widgets/staff_row.dart
@@ -0,0 +1,71 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:sdur_app/util/device_info.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class StaffRow extends StatelessWidget {
+  final String imageUrl;
+  final String jobTitle;
+  final String name;
+  final String email;
+  final String phone;
+
+  const StaffRow({
+    this.imageUrl,
+    this.jobTitle,
+    this.name,
+    this.email,
+    this.phone,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () async => await launch("mailto:$email"),
+          child: Row(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  fit: BoxFit.cover,
+                  image: imageUrl,
+                  width: DeviceInfo.width(context) * 0.35,
+                  height: DeviceInfo.height(context) * 0.125,
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, top: 16, bottom: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      _buildAutoSizeText(context, jobTitle, isBold: true),
+                      _buildAutoSizeText(context, name),
+                      _buildAutoSizeText(context, email),
+                      _buildAutoSizeText(context, phone)
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(height: 16),
+      ],
+    );
+  }
+
+  Widget _buildAutoSizeText(BuildContext ctx, String text, {bool isBold = false}) {
+    final TextStyle _textStyle = Theme.of(ctx).textTheme.body1.copyWith(height: 1.2);
+
+    return AutoSizeText(
+      text,
+      style: _textStyle.copyWith(fontWeight: isBold ? FontWeight.bold : FontWeight.normal),
+      maxLines: 1,
+    );
+  }
+}
